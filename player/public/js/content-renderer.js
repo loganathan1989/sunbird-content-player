d5f6dcc98068d376f7399f6bd1ffd2a399469f46
Akash Gupta <akash.gupta@tarento.com>
2018-09-14 11:04:38 +0530

    Issue #SB-5940 fix: Show Error when previewURL is not available

--- player/public/js/content-renderer.js
+++ player/public/js/content-renderer.js
@@ -354,6 +354,9 @@ org.ekstep.contentrenderer.device = function() {
 org.ekstep.contentrenderer.getBasePath = function(contentObj) {
     var config = EkstepRendererAPI.getGlobalConfig();
     var basePath = config.basepath || contentObj.baseDir;
+    if (config.context.streaming && !content.previewUrl) {
+        showToaster('error', "Content Stream Url is not available");
+    }
     switch (contentObj.mimeType) {
         case window.mimeType.ecml:
             if (config.context.streaming) {
